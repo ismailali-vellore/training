ec5886be1a20bf90158491d0c2fd0b35383e1f57
sajjuhussain <sajjadhussain.it24@gmail.com>
2022-10-05 11:58:00 +0530

    Prg

---
+++ Primenumber1.java
@@ -0,0 +1,19 @@
+package Program;
+
+public class Primenumber1 {
+	
+
+	public static void main(String[] args) {
+		int n=12;
+		if (n%2==0)
+		{
+			System.out.println("Number not a Prime"+n);
+		}
+		else {
+			System.out.println("Number is a Prime"+n);
+		}
+		
+
+	}
+
+}
